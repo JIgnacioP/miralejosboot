68a617e479e29531dae37d476801e72356f90745
JIgnacioP <130513861+JIgnacioP@users.noreply.github.com>
2023-06-01 14:21:13 -0300

    Add files via upload
    Alert en Java

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
-console.log('SELECCIONE UNA OPCION');
-console.log('1 - Acepto');
-console.log('2 - No Acepto');
 codingres = Number(prompt('¿Acepta Suscribirse a Nuestro Newsletter?(1.Si / 2.No):'));
 switch (codingres) {
 case 1:
-console.log('GRACIAS, por suscribirse a nuestro Newsletter');
+alert('GRACIAS, por suscribirse a nuestro Newsletter');
 break;
 case 2:
-console.log('Lamentamos su seleccion. Tal Vez en otra oportunidad.');
+alert('Lamentamos su seleccion. Tal Vez en otra oportunidad.');
 break;
 default:
-console.log('Por favor ingrese una opcion valida.');
+alert('Por favor ingrese una opcion valida.');
 }
